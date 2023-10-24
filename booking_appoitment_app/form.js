62339abf883e03d45eeb386551367d0508d96151
shivangijadeja <shivangijadeja99@gmail.com>
2023-10-24 18:03:24 +0530

    [ADD] Here I added code for read data from object with json stringify

--- booking_appoitment_app/form.js
+++ booking_appoitment_app/form.js
@@ -10,7 +10,15 @@ function storeuser(e){
         e.preventDefault();
         var u_name=document.querySelector('.user_name').value;
         var u_email=document.querySelector('.user_email').value;
-        localStorage.setItem('User_details',(u_name.concat(':',u_email)));
+        // localStorage.setItem('User_details',(u_name.concat(':',u_email)));
+        var user={
+            'User name':u_name,
+            'User email':u_email
+        }
+        var user_serialize=JSON.stringify(user);
+        localStorage.setItem('User Details',user_serialize)
+        var user_deserilize=JSON.parse(localStorage.getItem('User Details'))
+        console.log(user_deserilize);
         document.querySelector('.user_name').value='';
         document.querySelector('.user_email').value='';
     }
