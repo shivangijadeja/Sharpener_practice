7fb5ca10f953e42bb56c33df6711481d8522290d
shivangijadeja <shivangijadeja99@gmail.com>
2023-10-25 11:03:49 +0530

    [ADD] Here I added functionality of store more users in local storage

--- booking_appoitment_app/form.js
+++ booking_appoitment_app/form.js
@@ -1,4 +1,6 @@
 var my_form=document.querySelector('#addForm');
+var item_list=document.getElementById('items');
+var users=[]
 
 my_form.addEventListener('submit',storeuser);
 
@@ -10,17 +12,34 @@ function storeuser(e){
         e.preventDefault();
         var u_name=document.querySelector('.user_name').value;
         var u_email=document.querySelector('.user_email').value;
-        // localStorage.setItem('User_details',(u_name.concat(':',u_email)));
+        var create_item=document.createElement('li')
+        create_item.className='list-group-item'
+        var item_text=document.createTextNode(u_name.concat("-", u_email))
+        create_item.appendChild(item_text);
+        item_list.appendChild(create_item);
+        
         var user={
             'User name':u_name,
             'User email':u_email
         }
-        var user_serialize=JSON.stringify(user);
-        localStorage.setItem('User Details',user_serialize)
-        var user_deserilize=JSON.parse(localStorage.getItem('User Details'))
-        console.log(user_deserilize);
+        users.push(user)
+        storeuser_item(users);
+        // localStorage.setItem('User_details',(u_name.concat(':',u_email)));
+        // var user_serialize=JSON.stringify(user);
+        // localStorage.setItem('User Details',user_serialize)
+        // var user_deserilize=JSON.parse(localStorage.getItem('User Details'))
+        // console.log(user_deserilize);
         document.querySelector('.user_name').value='';
         document.querySelector('.user_email').value='';
     }
     
+}
+function storeuser_item(users) {
+    for(var i=0;i<users.length;i++){
+        console.log(users[i])
+        serialize_data=JSON.stringify(users[i])
+        deserialize_data=JSON.parse(serialize_data)
+        localStorage.setItem(deserialize_data['User name'], serialize_data )
+        
+    }
 }
