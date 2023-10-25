b675266c2ba8db3f53794f7d6c866340d90f8ded
shivangijadeja <shivangijadeja99@gmail.com>
2023-10-25 12:38:51 +0530

    [ADD] Here I added functionality to edit users from UI and local storage

--- booking_appoitment_app/form.js
+++ booking_appoitment_app/form.js
@@ -1,10 +1,13 @@
 var my_form=document.querySelector('#addForm');
 var item_list=document.getElementById('items');
+
 var users=[]
 
 my_form.addEventListener('submit',storeuser);
 
 item_list.addEventListener('click',removeitem);
+
+
 
 function storeuser(e){
     if(document.querySelector('.user_email').value==='' || document.querySelector('.user_name').value===''){
@@ -19,8 +22,14 @@ function storeuser(e){
         var item_text=document.createTextNode(u_name.concat("-", u_email))
         create_item.appendChild(item_text);
 
+        var btn_edit=document.createElement('button');
+        btn_edit.className='btn btn-info btn-sm mr-2 float-right edit_button'
+        var btn_edit_text=document.createTextNode('Edit');
+        btn_edit.appendChild(btn_edit_text);
+        create_item.appendChild(btn_edit)
+
         var btn_del=document.createElement('button');
-        btn_del.className='btn btn-danger btn-sm float-right delete';
+        btn_del.className='btn btn-danger btn-sm mr-2 float-right delete';
         var btn_text=document.createTextNode('Delete');
         btn_del.appendChild(btn_text)
         create_item.appendChild(btn_del);
@@ -53,12 +62,19 @@ function storeuser_item(users) {
     }
 }
 function removeitem(e){
+    var li=e.target.parentElement;
+    var selected_name=li.innerText.split('-')[0];
+    var selected_email=(li.innerText.split('-')[1]).split('\n')[0]
     if(e.target.classList.contains('delete')){
         if(confirm('Are you sure?')){
-            var li=e.target.parentElement;
             item_list.removeChild(li);
-            var selected_name=li.innerText.split('-')[0];
             localStorage.removeItem(selected_name);
         }
+    }
+    if(e.target.classList.contains('edit_button')){
+        document.querySelector('.user_name').value=selected_name;
+        document.querySelector('.user_email').value=selected_email;
+        localStorage.removeItem(selected_name);
+        item_list.removeChild(li);
     }
 }
