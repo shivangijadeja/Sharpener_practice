21ca037c5b22234c8de02e02c7e8777140140947
shivangijadeja <shivangijadeja99@gmail.com>
2023-11-02 10:48:08 +0530

    [MOD] Modified booking appoitment app for fetch data from crud crud

--- booking_appoitment_app/form.js
+++ booking_appoitment_app/form.js
@@ -1,5 +1,6 @@
 var my_form=document.querySelector('#addForm');
 var item_list=document.getElementById('items');
+var body=document.querySelector('body')
 
 var users=[]
 
@@ -81,4 +82,38 @@ function removeitem(e){
         localStorage.removeItem(selected_name);
         item_list.removeChild(li);
     }
+}
+
+window.onload = (event) => {
+    axios.get("https://crudcrud.com/api/37670e6543cb4ce5bcd6a954d07dd3d8/studentdata")
+    .then((res)=>{
+        for(var i=0;i<res.data.length;i++){
+            showusers(res.data[i])
+        }
+        console.log(res.data)
+    })
+    .catch((err)=>{console.log(err)})
+};
+
+function showusers(user){
+    var u_name=user['u_name'];
+    var u_email=user['u_email'];
+    var create_item=document.createElement('li')
+    create_item.className='list-group-item'
+    var item_text=document.createTextNode(u_name.concat("-", u_email))
+    create_item.appendChild(item_text);
+
+    var btn_edit=document.createElement('button');
+    btn_edit.className='btn btn-info btn-sm mr-2 float-right edit_button'
+    var btn_edit_text=document.createTextNode('Edit');
+    btn_edit.appendChild(btn_edit_text);
+    create_item.appendChild(btn_edit)
+
+    var btn_del=document.createElement('button');
+    btn_del.className='btn btn-danger btn-sm mr-2 float-right delete';
+    var btn_text=document.createTextNode('Delete');
+    btn_del.appendChild(btn_text)
+    create_item.appendChild(btn_del);
+
+    item_list.appendChild(create_item);
 }
