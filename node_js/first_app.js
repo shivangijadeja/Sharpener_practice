3ded989291888a1ddf499b5a417a7798aa045e00
shivangijadeja <shivangijadeja99@gmail.com>
2023-11-17 16:55:25 +0530

    [ADD] Added code for add code based on different urls

--- node_js/first_app.js
+++ node_js/first_app.js
@@ -2,6 +2,28 @@
 
 const http=require('http')
 const server=http.createServer((req,res)=>{
-    console.log("Shivangi")
+    // console.log("Shivangi")
+    const url=req.url
+    if(url==='/home'){
+        res.write('<html>')
+        res.write('<head><title>My home page</title></head>')
+        res.write('<body><h1>Welcome home</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
+    else if(url==='/about'){
+        res.write('<html>')
+        res.write('<head><title>My about us page</title></head>')
+        res.write('<body><h1>Welcome to about us page</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
+    else if(url==='/node' || url==='/'){
+        res.write('<html>')
+        res.write('<head><title>My node page</title></head>')
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+        res.write('</html>')
+        res.end()
+    }
 })
 server.listen(4000)
