b021f1e4f286be6242e5a52adfdc15fe3db528cf
shivangijadeja <shivangijadeja99@gmail.com>
2023-11-17 15:16:06 +0530

    [ADD] Added code for creating server with given port

--- node_js/first_app.js
+++ node_js/first_app.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+const http=require('http')
+const server=http.createServer((req,res)=>{
+    console.log("Shivangi")
+})
+server.listen(4000)
