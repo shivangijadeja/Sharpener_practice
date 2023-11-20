e3931d3a21375618fd406f8c6c31cb6afbc7292f
shivangijadeja <shivangijadeja99@gmail.com>
2023-11-20 12:07:56 +0530

    [ADD] Added code for add form with differnt url and get that data in logs by body parser

--- node_js/app.js
+++ node_js/app.js
@@ -1,13 +1,26 @@
 const http=require('http')
 const express=require('express')
 const app=express()
-app.use((req,res,next)=>{
-    console.log('in the middleware')
+const bodyParser=require('body-parser')
+
+app.use(bodyParser.urlencoded());
+
+app.use('/',(req,res,next)=>{
+    console.log('This middleware calls always')
     next(); //it allows the request to continue to the next middleware in line
 })
-app.use((req,res,next)=>{
-    console.log('in the another middleware')
-    res.send("<h1>HELLO FROM EXPRESS JS</h1>")
+app.use('/add_product',(req,res,next)=>{
+    res.send("<form action='/product' method='POST'><input type='text' name='title'><br/><input type='number' name='size'> <br/> <button type='submit'>Add product</button></form>")
 })
-const server=http.createServer(app)
-server.listen(4005)
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>HELLO</h1>')
+})
+// const server=http.createServer(app)
+// server.listen(4005)
+app.listen(4005)
