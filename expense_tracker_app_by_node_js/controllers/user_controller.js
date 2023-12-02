b80b8058f443bbe1ce54c6a16e73061694506a37
shivangijadeja <shivangijadeja99@gmail.com>
2023-12-02 19:37:15 +0530

    [ADD] Added code for make password encrypted

--- expense_tracker_app_by_node_js/controllers/user_controller.js
+++ expense_tracker_app_by_node_js/controllers/user_controller.js
@@ -1,14 +1,21 @@
 const db=require('../utils/database')
+const bcrypt=require('bcrypt'); 
 
 const addUser=(req,res)=>{
-    const u_name = req.body.user_name;
-    const email = req.body.email;
-    const pwd=req.body.password;
-    db.execute('INSERT INTO USER (user_name,email,password)  VALUES (?,?,?)' , [u_name,email,pwd]).then(
-        (res)=>{
-            console.log(res)
-        }
-    ).catch((err)=>console.log(err))
+    try{
+        const u_name = req.body.user_name;
+        const email = req.body.email;
+        const pwd=req.body.password;
+        const saltrounds=5 
+        bcrypt.hash(pwd,saltrounds,async(err,hash)=>{
+            let password={pwd:hash}
+            await db.execute('INSERT INTO USER (user_name,email,password)  VALUES (?,?,?)' , [u_name,email,password.pwd])
+            res.status(201).json({message:"Successfully user createds"})
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
 }
 
 const getAllUser=(req,res)=>{
@@ -22,12 +29,17 @@ const testUser=async (req,res)=>{
     const pwd=req.body.password;
     const result=await db.execute(`select * from user where email='${email}'`)
     if(result[0].length>0){
-        if(result[0][0].password===pwd){
-            res.status(200).send("User login succesfully");
-        }
-        else{
-            res.status(401).send("User not authorised");
-        }
+        bcrypt.compare(pwd,result[0][0].password,(err,response)=>{
+            if(err){
+                res.status(500).send("Something went wrong")
+            }
+            if(response==true){
+                res.status(200).send("User login succesfully");
+            }
+            else{
+                res.status(401).send("User not authorised");
+            }
+        })
     }
     else{
         res.status(404).send("User not found");
