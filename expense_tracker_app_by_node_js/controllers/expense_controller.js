00a1766365fba909bc3cae2606bec42d82523f74
shivangijadeja <shivangijadeja99@gmail.com>
2023-12-04 22:00:55 +0530

    [ADD] Optimised code of show leadership board

--- expense_tracker_app_by_node_js/controllers/expense_controller.js
+++ expense_tracker_app_by_node_js/controllers/expense_controller.js
@@ -7,7 +7,12 @@ const addExpense=(req,res)=>{
     const category=req.body.category;
     const user_id=req.user
     db.execute('INSERT INTO EXPENSE (amount,description,category,user_id)  VALUES (?,?,?,?)' , [amount,description,category,user_id])
-    res.status(201).json({message:"Successfully expense created"})
+    db.execute(`select total_expense from user where id=${user_id}`).then((total_expense)=>{
+        const previous_expense=total_expense[0][0].total_expense
+        db.execute(`update user set total_expense=${previous_expense}+${amount} where id=${user_id}`)
+    }).then(()=>{
+        res.status(201).json({message:"Successfully expense created"})
+    })
 }
 
 const getAllExpense=(req,res)=>{
@@ -23,7 +28,8 @@ const deleteExpense=(req,res)=>{
 }
 
 const showLeaderboard=(req,res)=>{
-    db.execute('select u.user_name,sum(e.amount) as total_expense from expense_tracker_app.expense e, expense_tracker_app.user u where e.user_id=u.id group by e.user_id order by sum(e.amount) desc;')
+    // db.execute('select u.user_name,sum(e.amount) as total_expense from expense_tracker_app.expense e, expense_tracker_app.user u where e.user_id=u.id group by e.user_id order by sum(e.amount) desc;')
+    db.execute('select user_name,total_expense from expense_tracker_app.user order by total_expense desc')
     .then((result)=>{
         res.status(200).json({expenses:result})
     }).catch((err)=>{
