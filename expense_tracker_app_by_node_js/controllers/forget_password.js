1101164899bf7b01d5c9f651edb607a205278220
shivangijadeja <shivangijadeja99@gmail.com>
2023-12-05 21:05:27 +0530

    [MOD] Modified code of backend for send mail

--- expense_tracker_app_by_node_js/controllers/forget_password.js
+++ expense_tracker_app_by_node_js/controllers/forget_password.js
@@ -1,33 +1,48 @@
-const Sib=require('sib-api-v3-sdk')
-require('dotenv').config()
+const Sib = require('sib-api-v3-sdk');
+const client = Sib.ApiClient.instance;
+client.authentications['api-key'].apiKey = process.env.API_KEY;
+const tranEmailApi = new Sib.TransactionalEmailsApi();
 
-const sendEmail=(req,res)=>{
-
-    const client=Sib.ApiClient.instance
-    const apikey=client.authentications['api-key']
-    apikey.apikey=process.env.API_KEY
-
-    const transEmailApi=new Sib.TransactionalEmailsApi()
-    const sender={
-        email:'shivangijadeja99@gmail.com'
+const sendEmail = async (req, res) => {
+    const sender = {
+        email: 'shivangijadeja99@gmail.com',
+        name: 'shivangi'
     }
-    const receiver=[
+    const receivers = [
         {
-            email:req.body.email
+            email: req.body.email
         }
     ]
-    transEmailApi.sendTransacEmail({
-        sender,
-        to:receiver,
-        subject:"Testing email",
-        textContent:" Testing this sending mail service"
-    }).then((res)=>{
-        console.log(res)
-    }).catch((err)=>{
-        console.log(err)
-    })
 
-}
+    try {
+        const mailresponse = await tranEmailApi.sendTransacEmail({
+            sender,
+            to: receivers,
+            subject: "Reset Your password",
+            htmlContent: `
+          <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Password Reset</title>
+            </head>
+            <body>
+                <h1>Reset Your Password</h1>
+                <p>Click the button below to reset your password:</p>
+                <button><a href="/password/reset/{{params.role}}">Reset Password</a></button>
+            </body>
+            </html>`, params: {
+                role: 5
+            }
+        })
+      return res.status(200).json({
+        message:
+          "Link for reset the password is successfully send on your Mail Id!",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(409).json({ message: "failed changing password" });
+    }
+  };
 
 module.exports={
     sendEmail,
