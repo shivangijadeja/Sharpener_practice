ea94e7380de361767b18581ec0a7c5238a8594e7
shivangijadeja <shivangijadeja99@gmail.com>
2023-12-05 11:12:40 +0530

    [ADD] Added code of frontend forget password

---
+++ expense_tracker_app_by_node_js/views/forget_password.js
@@ -0,0 +1,11 @@
+let my_form=document.querySelector('.forget_password_form');
+
+my_form.addEventListener('submit',forgetPassword);
+
+function forgetPassword(e){
+    e.preventDefault();
+    let user={
+        "email": document.querySelector('.u_email').value
+    }
+    axios.post('http://localhost:8000/password/forgotpassword',user)
+}
