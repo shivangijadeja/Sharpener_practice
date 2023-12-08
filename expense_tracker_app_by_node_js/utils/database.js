37f17b4c87a9b05bd4e0f2d7546a3a520362b72d
shivangijadeja <shivangijadeja99@gmail.com>
2023-12-08 15:56:16 +0530

    [MOD] Made code ready for deployment

--- expense_tracker_app_by_node_js/utils/database.js
+++ expense_tracker_app_by_node_js/utils/database.js
@@ -1,10 +1,10 @@
 const mysql=require('mysql2')
 
 const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'expense_tracker_app',
-    password:'Shivangi@9909'
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    database:process.env.DB_DATABASE,
+    password:process.env.DB_PASSWORD
 })
 
 module.exports=pool.promise();
