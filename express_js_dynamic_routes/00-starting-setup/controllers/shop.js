d64dac2923ed03301c45589d2b9729ef03c73fa9
shivangijadeja <shivangijadeja99@gmail.com>
2023-12-29 10:04:41 +0530

    [ADD] Added code for add order and display orders by nosql

--- express_js_dynamic_routes/00-starting-setup/controllers/shop.js
+++ express_js_dynamic_routes/00-starting-setup/controllers/shop.js
@@ -70,11 +70,25 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
+exports.postOrder=(req,res,next)=>{
+  req.user.addOrder()
+  .then(result=>{
+    res.redirect('/orders');
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+}
+
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user.getOrders().then((orders)=>{
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders:orders
+    });
+  })
+  
 };
 
 exports.getCheckout = (req, res, next) => {
--- express_js_dynamic_routes/00-starting-setup/models/user.js
+++ express_js_dynamic_routes/00-starting-setup/models/user.js
@@ -71,6 +71,32 @@ class User{
       )
   }
 
+  addOrder(){
+    const db=getDb()
+    return this.getCart().then(products=>{
+      const order={
+        items:products,
+        user:{
+          _id:new mongodb.ObjectId(this._id),
+          name:this.name
+        }
+      }
+      return db.collection('orders').insertOne(order)
+    })
+    .then(result=>{
+      this.cart={items:[]};
+      return db.collection('users').updateOne(
+        {_id:new mongodb.ObjectId(this._id)},
+        {$set:{cart:{items:[]}}}
+        )
+    })
+  }
+
+  getOrders(){
+    const db=getDb()
+    return db.collection("orders").find({"user._id":new mongodb.ObjectId(this._id)}).toArray()
+  }
+
   static findUserById(u_id){
     const db=getDb()
     return db.collection('users').findOne({_id:new mongodb.ObjectId(u_id)})
